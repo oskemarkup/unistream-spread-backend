d10451f7997fd80fcff9ac5e737efcff3d99fc23
oske <oske.design@gmail.com>
2023-01-24 15:40:50 +0500

    fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
@@ -16,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors())
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
